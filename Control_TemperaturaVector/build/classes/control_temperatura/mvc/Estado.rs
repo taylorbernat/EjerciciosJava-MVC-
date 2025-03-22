dda261ae82dc0934caf1c2de52651408212f4d9f
taylorbernat <taylorfabianbernate@gmail.com>
2025-03-22 13:19:37 -0500

    Delete Control_TemperaturaVector directory

--- Control_TemperaturaVector/build/classes/control_temperatura/mvc/Estado.rs
+++
@@ -1,6 +0,0 @@
-control_temperatura.mvc.Sensor
-control_temperatura.mvc.Ventilador
-control_temperatura.mvc.Modelo
-control_temperatura.mvc.Estado
-control_temperatura.mvc.Calefactor
-control_temperatura.mvc.Inactivo
